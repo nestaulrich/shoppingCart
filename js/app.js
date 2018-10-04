005b816bad04d526c4e377be16a05716d736c5b1
Greg Ulrich <nestaulrich@gmail.com>
2018-10-04 15:51:33 -0400

    Add selection function to read course info

--- js/app.js
+++ js/app.js
@@ -4,6 +4,27 @@ const courses = document.querySelector('#courses-list');
 
 // Event Listeners
 
-runEventListeners();
+loadEventListeners();
+
+function loadEventListeners() {
+    courses.addEventListener('click' , buyCourse);
+
+}
 
 // Functions
+
+function buyCourse(e) {
+    //prevents the default behavior of the form element
+    e.preventDefault();
+    //Use delegation to find the course that was added to the shopping cart
+    if (e.target.classList.contains('add-to-cart') ) {
+        const course = e.target.parentElement.parentElement;
+
+        getCourseInfo(course);
+    }
+}
+
+// Reads the HTML information of the selection
+function getCourseInfo(course) {
+    console.log( course);
+}
